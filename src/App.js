4a56aeb9dcd2133b887c28dca0977a4f6d7d95e6
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-03 10:43:45 +0000

    api calls being made but response not expected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  function handleClick() {
+  fetch("https://api-football-v1.p.rapidapi.com/v2/predictions/157462", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+		"x-rapidapi-key": "ae5e938ba4mshed60cad0dd964c7p118373jsnf418cc6bee48"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+});}
+function handleOtherClick() {
+  fetch("https://api-football-v1.p.rapidapi.com/v2/leagues", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+		"X-RapidAPI-Key": "ae5e938ba4mshed60cad0dd964c7p118373jsnf418cc6bee48"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+});
+}
   return (
     <div className="App">
       Football App
@@ -10,6 +39,9 @@ function App() {
         <option>team1</option>
         <option>team2</option>
       </select>
+      <button onClick={handleClick}>Test API</button>
+      <button onClick={handleOtherClick}>Test API</button>
+      
     </div>
   );
 }
