5b3599af3befb12e3f601ad5b279fabbc777813d
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-05 16:58:41 +0000

    generating random teams on backend database

--- src/Backend/App.js
+++ src/Backend/App.js
@@ -3,9 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 // const DB = require('./database.js')
-const test = require('./test.js')
+const {test, getTeam} = require('./test.js')
+
 
 app.get('/', (req, res) => res.send('Football app!'))
 // app.get('/rest/db', (req, res) => res.send(DB()))
 app.get('/rest/test', (req, res) => res.send(test()))
+app.get('/rest/test/1', (req, res) => 
+    {let team = getTeam(Math.floor(Math.random() * Math.floor(5)) + 1); res.send(team)})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/Backend/test.js
+++ src/Backend/test.js
@@ -1,6 +1,18 @@
-module.exports = function test() {
+module.exports = {
+    test: test,
+    getTeam: getTeam
+}
+function test() {
     return [
-        {id: '1', name: 'Arsenal'},
-        {id: '2', name: 'Chelsea'}
+        {id: 1, name: 'Arsenal'},
+        {id: 2, name: 'Chelsea'},
+        {id: 3, name: 'Aston villa'},
+        {id: 4, name: 'Liverpool'},
+        {id: 5, name: 'Norwich'}
     ];
+}
+
+function getTeam(id) {
+    let teams = test();
+    return teams.filter(team => team.id === id);
 }
