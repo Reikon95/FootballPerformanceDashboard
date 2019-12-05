9d4e8626753885cae1f89558aa2754a6c944aa27
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-05 17:40:32 +0000

    data passes from be 2 fe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import StandingsDatabase from './StandingsDatabase.js'
 function App() {
-
+  let [teams, setTeams] = useState([]) 
+  useEffect(() => {
+    fetch('/rest/test')
+    .then(response => response.json())
+    .then(teams => {
+      console.log('The current list of teams is ', teams)
+    })
+  }, []);
   return (
     <>
     <h2>Next Fixture Differential</h2>
-    <h2>Current Form</h2>
+    {/* <h2>Current Form</h2>
     <h2>Top Players</h2>
     <h2>Goals Scored</h2>
     <h2>Goals Conceded</h2>
-    <StandingsDatabase/>
+    <StandingsDatabase/> */}
+
     </>
   )
 }
--- src/Backend/App.js
+++ src/Backend/App.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 // const DB = require('./database.js')
 const {test, getTeam} = require('./test.js')
 
@@ -9,6 +9,7 @@ const {test, getTeam} = require('./test.js')
 app.get('/', (req, res) => res.send('Football app!'))
 // app.get('/rest/db', (req, res) => res.send(DB()))
 app.get('/rest/test', (req, res) => res.send(test()))
-app.get('/rest/test/1', (req, res) => 
+app.get('/rest/test/:id', (req, res) => 
     {let team = getTeam(Math.floor(Math.random() * Math.floor(5)) + 1); res.send(team)})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
