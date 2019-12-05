acdb5c4e9264c317094f691217edda5bca0d22eb
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-05 17:59:57 +0000

    .

--- src/Backend/test.js
+++ src/Backend/test.js
@@ -6,7 +6,7 @@ function test() {
     return [
         {id: 1, name: 'Arsenal'},
         {id: 2, name: 'Chelsea'},
-        {id: 3, name: 'Aston villa'},
+        {id: 3, name: 'Aston Villa'},
         {id: 4, name: 'Liverpool'},
         {id: 5, name: 'Norwich'}
     ];
