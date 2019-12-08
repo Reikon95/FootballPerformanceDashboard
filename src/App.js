566894cb9918531f532c9cf5980d5c9dc682a3d8
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-08 16:52:01 +0000

    added a landing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import StandingsDatabase from './StandingsDatabase.js'
+import LandingPage from './LandingPage.js';
 function App() {
-  let [teams, setTeams] = useState([]) 
-  useEffect(() => {
-    fetch('/rest/test')
-    .then(response => response.json())
-    .then(teams => setTeams(teams))
-  }, []);
   return (
     <>
-
+    Current Selectors
+    <LandingPage/>
     Real standings
     <StandingsDatabase/>
+    
     </>
   )
 }
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios';
 import './StandingsDBStyle.css';
-function StandingsDatabase() {
+function LandingPage() {
   const teamDatabase = [];
   const [info, setInfo] = useState([]);
+  const [selectedTeam, setSelectedTeam] = useState('');
   useEffect(() => {
     Axios.get('http://api.football-data.org/v2/competitions/2021/standings', {
       headers: {'X-Auth-Token' : 'ffdf6b8895ad49d3af4db2724aea4c3b', 'Content-Type': 'application/json'},
@@ -12,23 +13,27 @@ function StandingsDatabase() {
       .then(data => setInfo(data.data.standings[0].table))
   }, [])
   for (let teams of info) {
-    console.log(teams.team.name);
     teamDatabase.push(teams.team.name);
   }
   teamDatabase.sort();
-  console.log(teamDatabase);
+  function handleClick(e) {
+    setSelectedTeam(`You've selected ${e.target.value}`)
+  }
       
   return (
     <>
-
-  
-    {info.map(team => (
-        <li>{team.position} {team.team.name} {team.playedGames} {team.won} {team.draw} {team.lost}  
-            {team.goalsFor} {team.goalsAgainst} {team.goalDifference} <strong>{team.points}</strong></li>
-    ))}
+    <h1>Welcome to the football app - anaylse your team's performance</h1>
+    <h3>Select your team here. Please note, the english premier league is currently the only supported league.</h3>
+    <select>
+      {teamDatabase.map(team => (
+        <option>{team}</option>
+      ))}
+    </select>
+    <button onCLick={()=>handleClick}>submit</button>
+    Your team is {selectedTeam}
     
     </>
   );
 }
 
-export default StandingsDatabase;
+export default LandingPage;
