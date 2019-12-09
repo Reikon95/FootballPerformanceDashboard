408a4219895b188ef0e151f4cf8b96b030cc4a63
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-09 15:21:12 +0000

    s

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 import StandingsDatabase from './StandingsDatabase.js'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './LandingPage.js';
 import Scorers from './Scorers.js'
 function App() {
   return (
+    <Router>
     <>
-    Current Selectors
-    <LandingPage/>
-    <StandingsDatabase/>
-    <Scorers/>
+    <Switch>
+      <Route exact path= '/' component={LandingPage}/>
+      <Route exact path= '/standings' component={StandingsDatabase}/>
+      <Route exact path= '/scorers' component={Scorers}/>
+    </Switch>
     </>
+    </Router>
   )
 }
 
--- src/Backend/App.js
+++ src/Backend/App.js
@@ -1,18 +1,12 @@
 
 const express = require('express')
 const app = express()
-const port = 3001
-// const DB = require('./database.js')
+const port = process.env.PORT || 3001
 const {test, getTeam} = require('./test.js')
 
 
 app.get('/', (req, res) => res.send('Football app!'))
-// app.get('/rest/db', (req, res) => res.send(DB()))
 app.get('/rest/test', (req, res) => res.send(test()))
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
 app.get('/rest/test/:id', (req, res) => 
     {let team = getTeam(Math.floor(Math.random() * Math.floor(5)) + 1); res.send(team)})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
