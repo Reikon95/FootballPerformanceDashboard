cdb5b06af6232018b4d8ab38d32b459c58d77a02
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-09 10:05:29 +0000

    scorers added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import StandingsDatabase from './StandingsDatabase.js'
 import LandingPage from './LandingPage.js';
+import Scorers from './Scorers.js'
 function App() {
   return (
     <>
     Current Selectors
     <LandingPage/>
-    
+    <StandingsDatabase/>
+    <Scorers/>
     </>
   )
 }
---
+++ src/Scorers.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState } from 'react';
+import Axios from 'axios';
+
+export default function Scorers() {
+    const [players, setPlayers] = useState([])
+    useEffect(() => {
+        Axios.get('http://api.football-data.org/v2/competitions/2021/scorers', {
+            headers: {'X-Auth-Token' : 'ffdf6b8895ad49d3af4db2724aea4c3b', 'Content-Type': 'application/json'},
+          })
+          .then(results => setPlayers(results))
+          console.log(players);
+          
+    })
+    return (
+    <p>Scores mounted</p>
+    )
+}
